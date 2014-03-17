930349e13e584876f861fdf2d23676c5196746dd
Evgeniy Plyusnin <eninbox@gmail.com>
2014-03-17 11:40:57 +0600

    В каталог блоков внесена информация о Монитор-КХ

--- FirmwarePacker/Properties/AssemblyInfo.cs
+++ FirmwarePacker/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.4.*")]
-[assembly: AssemblyFileVersion("0.4.1.0")]
+[assembly: AssemblyFileVersion("0.4.2.0")]
