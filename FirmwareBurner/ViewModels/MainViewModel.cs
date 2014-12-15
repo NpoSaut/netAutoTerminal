eb29104074d41d1982bc0e13329e1ae39b37f6bd
Evgeniy Plyusnin <eninbox@gmail.com>
2014-12-15 17:26:45 +0500

    Как-то переразбил на ViewModel-и проекта и таргет-селектора

--- FirmwareBurner/ViewModels/MainViewModel.cs
+++ FirmwareBurner/ViewModels/MainViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using FirmwareBurner.Events;
-using FirmwareBurner.Project;
 using FirmwareBurner.ViewModels.Bases;
 using FirmwareBurner.ViewModels.Targeting;
 using Microsoft.Practices.Prism.Events;
@@ -8,42 +7,26 @@ namespace FirmwareBurner.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        //private readonly IBurningReceipt _burnManager;
-        private readonly IBurningViewModelProvider _burningViewModelProvider;
-        private readonly IFirmwareProjectFactory _firmwareProjectFactory;
-        private readonly IProjectViewModelProvider _projectViewModelProvider;
-        private readonly IProjectValidatorViewModelProvider _validatorViewModelProvider;
+        private readonly ProjectManagerViewModelFactory _projectManagerViewModelFactory;
 
-        public MainViewModel(TargetSelectorViewModel TargetSelector, IEventAggregator EventAggregator, IProjectViewModelProvider ProjectViewModelProvider,
-                             IBurningViewModelProvider BurningViewModelProvider, IFirmwareProjectFactory FirmwareProjectFactory,
-                             IProjectValidatorViewModelProvider ValidatorViewModelProvider, FileRequestServiceViewModel FileRequestServiceViewModel)
+        public MainViewModel(TargetSelectorViewModel TargetSelector, IEventAggregator EventAggregator, FileRequestServiceViewModel FileRequestServiceViewModel,
+                             ProjectManagerViewModelFactory ProjectManagerViewModelFactory)
         {
             this.TargetSelector = TargetSelector;
-            _projectViewModelProvider = ProjectViewModelProvider;
-            _burningViewModelProvider = BurningViewModelProvider;
-            _firmwareProjectFactory = FirmwareProjectFactory;
-            _validatorViewModelProvider = ValidatorViewModelProvider;
             this.FileRequestServiceViewModel = FileRequestServiceViewModel;
+            _projectManagerViewModelFactory = ProjectManagerViewModelFactory;
 
             EventAggregator.GetEvent<TargetSelectedEvent>().Subscribe(OnTargetSelected);
         }
 
         public FileRequestServiceViewModel FileRequestServiceViewModel { get; private set; }
         public TargetSelectorViewModel TargetSelector { get; private set; }
-        public ProjectViewModel Project { get; private set; }
-        public BurningViewModel Burning { get; private set; }
-        public ProjectValidatorViewModel Validator { get; private set; }
+        public ProjectManagerViewModel ProjectManager { get; private set; }
 
         private void OnTargetSelected(TargetSelectedArgs SelectedArgs)
         {
-            Project = _projectViewModelProvider.GetViewModel(SelectedArgs.CellKindId, SelectedArgs.ModificationId);
-            Burning = _burningViewModelProvider.GetViewModel(SelectedArgs.CellKindId, SelectedArgs.ModificationId,
-                                                             new ViewModelProjectAssembler(Project, _firmwareProjectFactory));
-            Validator = _validatorViewModelProvider.GetViewModel(Project);
-
-            RaisePropertyChanged("Project");
-            RaisePropertyChanged("Burning");
-            RaisePropertyChanged("Validator");
+            ProjectManager = _projectManagerViewModelFactory.GetViewModel(SelectedArgs.CellKindId, SelectedArgs.ModificationId);
+            RaisePropertyChanged("ProjectManager");
         }
     }
 }
