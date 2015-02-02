cd8b0265258d68d7bbc64e6293235a52ece8dc94
Evgeniy Plyusnin <eninbox@gmail.com>
2015-02-02 18:45:55 +0500

    Для RepositoryFirmwareSelectorView добавлена SampleData

--- FirmwareBurner/ViewModels/SampleData.cs
+++ FirmwareBurner/ViewModels/SampleData.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using FirmwareBurner.ViewModels.Dialogs;
 using FirmwareBurner.ViewModels.FirmwareSources;
 using FirmwareBurner.ViewModels.Targeting;
 using FirmwarePacking;
+using FirmwarePacking.Repositories;
 
 namespace FirmwareBurner.ViewModels
 {
@@ -41,6 +44,11 @@ namespace FirmwareBurner.ViewModels
         private static readonly FirmwareSelectorDialogViewModel _firmwareSelectorDialog =
             new FirmwareSelectorDialogViewModel(_firmwareSelector);
 
+        private static readonly IRepository _fakeRepository = new FakeRepository();
+
+        private static readonly RepositoryFirmwareSelectorViewModel _repositoryFirmwareSelector =
+            new RepositoryFirmwareSelectorViewModel("Из репозитория", _fakeRepository, new ComponentTarget[0]);
+
         public static CompositeFirmwareSelectorViewModel FirmwareSelector
         {
             get { return _firmwareSelector; }
@@ -61,6 +69,11 @@ namespace FirmwareBurner.ViewModels
             get { return _firmwareSelectorDialog; }
         }
 
+        public static RepositoryFirmwareSelectorViewModel RepositoryFirmwareSelector
+        {
+            get { return _repositoryFirmwareSelector; }
+        }
+
         #region Fake Classes
 
         private class FakeFirmwareSelectorViewModel : FirmwareSelectorViewModel
@@ -71,6 +84,48 @@ namespace FirmwareBurner.ViewModels
             public override FirmwarePackage SelectedPackage
             {
                 get { return new FirmwarePackage { Information = _packageInformation }; }
+            }
+        }
+
+        private class FakeRepository : Repository
+        {
+            private readonly IEnumerable<IRepositoryElement> _packages =
+                Enumerable.Range(0, 6)
+                          .Reverse()
+                          .Select(i => new FakeRepositoryElement(new PackageInformation
+                                                                 {
+                                                                     FirmwareVersion = new Version(1, i),
+                                                                     FirmwareVersionLabel = "LDP",
+                                                                     ReleaseDate = DateTime.Today.AddHours(12).AddMonths(-i)
+                                                                 }))
+                          .ToList();
+
+            /// <summary>Список всех пакетов в репозитории</summary>
+            public override IEnumerable<IRepositoryElement> Packages
+            {
+                get { return _packages; }
+            }
+
+            private class FakeRepositoryElement : IRepositoryElement
+            {
+                public FakeRepositoryElement(PackageInformation Information)
+                {
+                    this.Information = Information;
+                    Targets = new[] { new ComponentTarget(1, 1, 1, 1) };
+                }
+
+                /// <summary>Информация о пакете</summary>
+                public PackageInformation Information { get; private set; }
+
+                /// <summary>Список целей, для которых имеются компоненты в данном пакете</summary>
+                public ICollection<ComponentTarget> Targets { get; private set; }
+
+                /// <summary>Загружает всё тело пакета</summary>
+                public FirmwarePackage GetPackage() { throw new NotImplementedException(); }
+
+                /// <summary>Загружает необходимый компонент из тела пакета</summary>
+                /// <param name="Target">Цель, компонент для которой требуется</param>
+                public FirmwareComponent GetComponent(ComponentTarget Target) { throw new NotImplementedException(); }
             }
         }
 
