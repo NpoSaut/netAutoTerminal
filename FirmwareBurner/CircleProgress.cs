e8d5261a48c816ff31b42cc2009d3a7a09e8f6ad
Evgeniy Plyusnin <eninbox@gmail.com>
2015-02-10 15:00:24 +0500

    Добавлен элемент управления CircleProgress

---
+++ FirmwareBurner/CircleProgress.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FirmwareBurner
+{
+    public class CircleProgress : ProgressBar
+    {
+        public static readonly DependencyProperty ProgressPortionProperty = DependencyProperty.Register(
+            "ProgressPortion", typeof (Double), typeof (CircleProgress),
+            new FrameworkPropertyMetadata(default(Double), FrameworkPropertyMetadataOptions.AffectsRender));
+
+        static CircleProgress() { DefaultStyleKeyProperty.OverrideMetadata(typeof (CircleProgress), new FrameworkPropertyMetadata(typeof (CircleProgress))); }
+
+        public Double ProgressPortion
+        {
+            get { return (Double)GetValue(ProgressPortionProperty); }
+            private set { SetValue(ProgressPortionProperty, value); }
+        }
+
+        protected override void OnValueChanged(double oldValue, double newValue)
+        {
+            base.OnValueChanged(oldValue, newValue);
+            UpdateProgressPortion();
+        }
+
+        protected override void OnMaximumChanged(double oldMaximum, double newMaximum)
+        {
+            base.OnMaximumChanged(oldMaximum, newMaximum);
+            UpdateProgressPortion();
+        }
+
+        private void UpdateProgressPortion() { ProgressPortion = Value / Maximum; }
+    }
+}
