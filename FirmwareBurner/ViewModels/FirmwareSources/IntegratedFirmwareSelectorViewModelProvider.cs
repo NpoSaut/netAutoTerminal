af2d50913c6fdbff1e308b66f281425735889702
Evgeniy Plyusnin <eninbox@gmail.com>
2015-02-16 11:53:51 +0500

    Теперь в IntegratedFirmwareSelector поставляются настоящие прошивки из папки

--- FirmwareBurner/ViewModels/FirmwareSources/IntegratedFirmwareSelectorViewModelProvider.cs
+++ FirmwareBurner/ViewModels/FirmwareSources/IntegratedFirmwareSelectorViewModelProvider.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using FirmwareBurner.Annotations;
-using FirmwareBurner.Fakes;
 using FirmwareBurner.ViewModels.FirmwareSources.LoadControllers;
 using FirmwarePacking;
 using FirmwarePacking.Repositories;
@@ -12,11 +11,12 @@ namespace FirmwareBurner.ViewModels.FirmwareSources
     public class IntegratedFirmwareSelectorViewModelProvider : IFirmwareSelectorViewModelProvider
     {
         private readonly ILoadControllerFactory _loadControllerFactory;
-        //private readonly Lazy<DirectoryRepository> _userDirectoryRepository;
+        private readonly DirectoryRepository _userDirectoryRepository;
+
         public IntegratedFirmwareSelectorViewModelProvider(ILoadControllerFactory LoadControllerFactory)
         {
             _loadControllerFactory = LoadControllerFactory;
-            //_userDirectoryRepository = _userDirectoryRepository = new Lazy<DirectoryRepository>(GetUserRepository); ;
+            _userDirectoryRepository = _userDirectoryRepository = new DirectoryRepository(DirectoryRepository.UserRepositoryDirectory);
         }
 
         public FirmwareSelectorViewModel GetViewModel(int CellKindId, int ModificationId, int ModuleId, int ChannelsCount)
@@ -25,18 +25,24 @@ namespace FirmwareBurner.ViewModels.FirmwareSources
                                                               .Select(channel => new ComponentTarget(CellKindId, ModificationId, channel, ModuleId))
                                                               .ToList();
 
+            //return new IntegratedFirmwareSelectorViewModel(
+            //    new[]
+            //    {
+            //        new BackgroundRepositoryLoader(new FakeRepository(0, 6, 500, false), requiredTargets)
+            //    },
+            //    new[]
+            //    {
+            //        new BackgroundRepositoryLoader(new FakeRepository(2, 8, 2000, true), requiredTargets)
+            //    },
+            //    _loadControllerFactory);
+
             return new IntegratedFirmwareSelectorViewModel(
                 new[]
                 {
-                    new BackgroundRepositoryLoader(new FakeRepository(0, 6, 500, false), requiredTargets)
+                    new BackgroundRepositoryLoader(_userDirectoryRepository, requiredTargets)
                 },
-                new[]
-                {
-                    new BackgroundRepositoryLoader(new FakeRepository(2, 8, 2000, true), requiredTargets)
-                },
+                new IRepositoryLoader[0],
                 _loadControllerFactory);
         }
-
-        private DirectoryRepository GetUserRepository() { return new DirectoryRepository(DirectoryRepository.UserRepositoryDirectory); }
     }
 }
