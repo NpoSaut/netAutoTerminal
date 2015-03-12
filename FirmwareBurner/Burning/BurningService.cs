717ba3703fbc5115c01d6b2d4d83ec7b1264e52d
Evgeniy Plyusnin <eninbox@gmail.com>
2015-03-12 16:41:35 +0500

    Как-то исправлена проблема с прогрессбаром

--- FirmwareBurner/Burning/BurningService.cs
+++ FirmwareBurner/Burning/BurningService.cs
@@ -12,11 +12,14 @@ namespace FirmwareBurner.Burning
     {
         private readonly IBurningReceiptsCatalog _burningReceiptsCatalog;
         private readonly IExceptionService _exceptionService;
+        private readonly IProgressControllerFactory _progressControllerFactory;
 
-        public BurningService(IBurningReceiptsCatalog BurningReceiptsCatalog, IExceptionService ExceptionService)
+        public BurningService(IBurningReceiptsCatalog BurningReceiptsCatalog, IExceptionService ExceptionService,
+                              IProgressControllerFactory ProgressControllerFactory)
         {
             _burningReceiptsCatalog = BurningReceiptsCatalog;
             _exceptionService = ExceptionService;
+            _progressControllerFactory = ProgressControllerFactory;
         }
 
         public ICollection<IBurningMethod> GetBurningMethods(string DeviceName)
@@ -29,21 +32,26 @@ namespace FirmwareBurner.Burning
 
         public void BeginBurn(IBurningReceipt Receipt, FirmwareProject Project, IProgressToken ProgressToken)
         {
-            Task.Factory.StartNew(() =>
-                                  {
-                                      try
-                                      {
-                                          Receipt.Burn(Project, ProgressToken);
-                                      }
-                                      catch (CreateImageException exception)
-                                      {
-                                          _exceptionService.PublishException("Не удалось составить образ для прошивки", exception.InnerException);
-                                      }
-                                      catch (BurningException exception)
-                                      {
-                                          _exceptionService.PublishException("Не удалось прошить устройство", exception.InnerException);
-                                      }
-                                  });
+            Task.Factory.StartNew(() => Burn(Receipt, Project, ProgressToken));
+        }
+
+        private void Burn(IBurningReceipt Receipt, FirmwareProject Project, IProgressToken ProgressToken)
+        {
+            using (_progressControllerFactory.CreateController(ProgressToken))
+            {
+                try
+                {
+                    Receipt.Burn(Project, ProgressToken);
+                }
+                catch (CreateImageException exception)
+                {
+                    _exceptionService.PublishException("Не удалось составить образ для прошивки", exception.InnerException);
+                }
+                catch (BurningException exception)
+                {
+                    _exceptionService.PublishException("Не удалось прошить устройство", exception.InnerException);
+                }
+            }
         }
     }
 }
