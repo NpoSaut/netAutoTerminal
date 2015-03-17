a858bbd4b6ac552690ba8c072704b80fe2492bd3
Evgeniy Plyusnin <eninbox@gmail.com>
2015-03-17 15:49:06 +0500

    Окошко без бордюров. С тенью. Можно таскать...

--- FirmwareBurner/Views/MainView.xaml.cs
+++ FirmwareBurner/Views/MainView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using FirmwareBurner.ViewModels;
 
 namespace FirmwareBurner.Views
@@ -10,6 +12,15 @@ namespace FirmwareBurner.Views
         {
             InitializeComponent();
             Loaded += (Sender, Args) => DataContext = ViewModel;
+        }
+
+        private void DragableElement_OnMouseDown(object Sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                Window window = Window.GetWindow(this);
+                if (window != null) window.DragMove();
+            }
         }
     }
 }
