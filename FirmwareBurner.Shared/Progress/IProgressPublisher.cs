49d1b708f357e5c87df117555d9ded4ad3a6fa3a
Evgeniy Plyusnin <eninbox@gmail.com>
2015-05-27 10:50:44 +0500

    Добавлен интерфейс IProgressPublisher и Proxy-класс ProgressProxy, преобразующий вызовы IProgressToken к событиям IProgressPublisher

---
+++ FirmwareBurner.Shared/Progress/IProgressPublisher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirmwareBurner.Progress
+{
+    public interface IProgressPublisher
+    {
+        event EventHandler Started;
+        event EventHandler Changed;
+        event EventHandler Compleated;
+
+        double Progress { get; }
+        bool IsIntermediate { get; }
+    }
+}
