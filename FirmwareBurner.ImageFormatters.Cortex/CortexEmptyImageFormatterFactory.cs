bae55e0e9c6c8e719e3f53361377c1c9d0dbd7e8
Evgeniy Plyusnin <eninbox@gmail.com>
2015-10-28 14:48:13 +0500

    Сделан пустой загрузчик для Cortex-ов

---
+++ FirmwareBurner.ImageFormatters.Cortex/CortexEmptyImageFormatterFactory.cs
@@ -0,0 +1,26 @@
+﻿using AsyncOperations.Progress;
+using FirmwareBurner.Burning;
+using FirmwareBurner.Imaging;
+using FirmwareBurner.Imaging.Binary.Buffers;
+
+namespace FirmwareBurner.ImageFormatters.Cortex
+{
+    public class CortexEmptyImageFormatterFactory : IImageFormatterFactory<CortexImage>
+    {
+        private readonly IBufferFactory _bufferFactory;
+        private readonly IProgressControllerFactory _progressControllerFactory;
+
+        public CortexEmptyImageFormatterFactory(IBufferFactory BufferFactory, IProgressControllerFactory ProgressControllerFactory)
+        {
+            _bufferFactory = BufferFactory;
+            _progressControllerFactory = ProgressControllerFactory;
+        }
+
+        /// <summary>Создаёт сборщик образа для указанного типа устройства</summary>
+        /// <param name="DeviceName">Тип устройства</param>
+        public IImageFormatter<CortexImage> GetFormatter(string DeviceName)
+        {
+            return new CortexEmptyImageFormatter(_progressControllerFactory, _bufferFactory);
+        }
+    }
+}
