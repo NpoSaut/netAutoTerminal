5f2ee19769f9d134837269798c1eeff20f71e786
Evgeniy Plyusnin <eninbox@gmail.com>
2015-11-25 16:43:20 +0500

    Подписывание инсталлятора!

--- BurnerInstaller/setup.cs
+++ BurnerInstaller/setup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using WixSharp;
 using WixSharp.CommonTasks;
+using WixSharp.Controls;
 using Assembly = System.Reflection.Assembly;
 using File = WixSharp.File;
 
@@ -27,22 +28,27 @@ public class Script
                                           items),
 
                                   // Shortcuts
-                                  new Dir(Path.Combine("%ProgramMenu%", company),
-                                          new ExeFileShortcut(projectName, "[INSTALL_DIR]" + exeFileName, "") { WorkingDirectory = "INSTALL_DIR" },
+                                  new Dir(@"%ProgramMenu%",
+                                          new ExeFileShortcut(projectName, "[INSTALLDIR]" + exeFileName, "") { WorkingDirectory = "INSTALLDIR" },
                                           new ExeFileShortcut("Удалить " + projectName, "[System64Folder]msiexec.exe", "/x [ProductCode]")
                                           {
                                               WorkingDirectory = "INSTALLDIR"
                                           }
                                       ),
                                   new Dir("%Desktop%",
-                                          new ExeFileShortcut(projectName, "[INSTALLDIR]" + exeFileName, "") { WorkingDirectory = "INSTALL_DIR" }
+                                          new ExeFileShortcut(projectName, "[INSTALLDIR]" + exeFileName, "") { WorkingDirectory = "INSTALLDIR" }
                                       ));
 
         File exeFile = project.ResolveWildCards(true)
                               .FindFile(f => f.Name.EndsWith(exeFileName))
                               .First();
 
+        //exeFile.AddShortcut(new FileShortcut(projectName, "%ProgramMenu%"));
+
         project.UI = WUI.WixUI_InstallDir;
+        project.CustomUI = new DialogSequence()
+            .On(NativeDialogs.WelcomeDlg, Buttons.Next, new ShowDialog(NativeDialogs.InstallDirDlg))
+            .On(NativeDialogs.InstallDirDlg, Buttons.Back, new ShowDialog(NativeDialogs.WelcomeDlg));
 
         project.Language = "ru-RU";
         project.Version = assembly.GetName().Version;
@@ -57,9 +63,22 @@ public class Script
         project.MajorUpgradeStrategy = MajorUpgradeStrategy.Default;
         project.OutDir = @"..\..\..\installers";
 
-        Compiler.BuildMsi(project);
-        Console.WriteLine("done");
-        Console.ReadLine();
+        string msi = Compiler.BuildMsi(project);
+
+        Console.WriteLine("Инсталлятор создан. Введите пароль к сертификату, чтобы подписать его или просто нажмите Enter, чтобы выйти:");
+        string password = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(password))
+            return;
+
+        int exitCode = Tasks.DigitalySign(msi,
+                                          "SautCodeSigning.pfx",
+                                          "http://timestamp.verisign.com/scripts/timstamp.dll",
+                                          password);
+
+        if (exitCode != 0)
+            Console.WriteLine("Could not sign the MSI file.");
+        else
+            Console.WriteLine("the MSI file was signed successfully.");
     }
 
     private static bool CheckFile(string file)
