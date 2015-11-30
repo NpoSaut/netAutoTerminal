2ef8edcbcf8e15cb78562c93a9f0ce56481a38bf
Evgeniy Plyusnin <eninbox@gmail.com>
2015-11-30 10:22:05 +0500

    Обработка ошибок при загрузке списка последних проектов, удаление недавних проектов, вызывающих ошибки при загрузке

--- FirmwarePacker/RecentProjects/IRecentProjectsService.cs
+++ FirmwarePacker/RecentProjects/IRecentProjectsService.cs
@@ -5,6 +5,7 @@ namespace FirmwarePacker.RecentProjects
     public interface IRecentProjectsService
     {
         IEnumerable<RecentProject> GetRecentProjects();
+        void RemoveRecentProject(string Path);
         void UpdateRecentProject(RecentProject Project);
     }
 }
--- FirmwarePacker/RecentProjects/SettingsRecentProjectsService.cs
+++ FirmwarePacker/RecentProjects/SettingsRecentProjectsService.cs
@@ -8,9 +8,11 @@ namespace FirmwarePacker.RecentProjects
     [UsedImplicitly]
     internal class SettingsRecentProjectsService : IRecentProjectsService
     {
-        private const int StoringCount = 10;
+        private const int StoringCount = 20;
 
         public IEnumerable<RecentProject> GetRecentProjects() { return Settings.Default.LastProjects ?? Enumerable.Empty<RecentProject>(); }
+
+        public void RemoveRecentProject(string Path) { Settings.Default.LastProjects.RemoveAll(p => p.FileName == Path); }
 
         public void UpdateRecentProject(RecentProject Record)
         {
@@ -18,11 +20,17 @@ namespace FirmwarePacker.RecentProjects
                 Settings.Default.LastProjects = new RecentProjectsList();
             Settings.Default.LastProjects.RemoveAll(p => p.FileName == Record.FileName);
             Settings.Default.LastProjects.Insert(0, Record);
-
-            //if (Settings.Default.LastProjects.Count > StoringCount)
-            //    Settings.Default.LastProjects.RemoveRange(StoringCount, Settings.Default.LastProjects.Count);
+            CleanUp();
 
             Settings.Default.Save();
+        }
+
+        private static void CleanUp()
+        {
+            int count = Settings.Default.LastProjects.Count;
+            if (count <= StoringCount)
+                return;
+            Settings.Default.LastProjects.RemoveRange(StoringCount, count - StoringCount);
         }
     }
 }
